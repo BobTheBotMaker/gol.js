3659bdd2ddbe579412406f4269e283a761725dc3
Andre Henry <ahenry@thinkthroughmath.com>
2014-07-11 16:28:43 -0400

    test all the rules

--- test/cell.test.js
+++ test/cell.test.js
@@ -9,4 +9,36 @@ describe('Cells', function(){
     var new_cell = cell.tick(1);
     assert.equal(new_cell.state, 'dead');
   })
+  
+  it('Should stay alive', function(){
+    var cell = new Cell('alive');
+    var new_cell = cell.tick(2);
+    assert.equal(new_cell.state, 'alive');
+    
+    var new_cell = cell.tick(3);
+    assert.equal(new_cell.state, 'alive');    
+  })
+  
+  it('Shold die if over populated', function(){
+    var cell = new Cell('alive');
+    var new_cell = cell.tick(4);
+    assert.equal(new_cell.state, 'dead');    
+  })
+
+  it('Shold come alive', function(){
+    var cell = new Cell('dead');
+    var new_cell = cell.tick(3);
+    assert.equal(new_cell.state, 'alive');    
+  })
+  
+  it('Shold stay dead', function(){
+    var cell = new Cell('dead');
+    var new_cell = cell.tick(2);
+    assert.equal(new_cell.state, 'dead');
+
+    var new_cell = cell.tick(4);
+    assert.equal(new_cell.state, 'dead');
+  })
+  
+  
 })
