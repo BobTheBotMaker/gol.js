a8be41c4052696fa91e09f634b078ba8a6b28289
Andre Henry <ahenry@thinkthroughmath.com>
2014-07-14 17:09:16 -0400

    Testing the rules

--- test/rules.test.js
+++ test/rules.test.js
@@ -3,23 +3,10 @@ var _ = require("underscore");
 
 describe('Rules', function(){
 
-  it('Should find a rule', function(){
+  it('Should be consistent', function(){
     var Rules = require("../lib/rules.js");
     var rules = new Rules().rules;
-
-    console.log(rules);
     
-    x = _.filter(rules, function(r){
-      return(r.initial == "alive")
-    });
-    
-    
-    console.log(x);
-    
-    for(var rule in rules){
-      console.log(rules[rule].condition());
-    }
-
   })
 
 })
